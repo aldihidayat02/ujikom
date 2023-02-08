c16730d08fed6c94626cbacb1a647e303434eb20
aldihidayat02 <aldihidayatt2005@gmail.com>
2023-02-08 07:44:18 +0700

    ujikomyuk

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login() {
+        return view('login');
+    }
+    
+    public function register(){
+        return view('register');
+    }
+
+    public function authenticating(Request $request){
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]); 
+
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+    }
+}
